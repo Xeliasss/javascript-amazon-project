c097e1ce18a0300a9ed5f75b214a95c1d151d30d
Xelias <bobkovdanil123@gmail.com>
2025-10-07 08:42:24 +0200

    test

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart, addToCart } from '../data/cart.js';
+import { cart, addToCart , calculateCartQuantity} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';      
 
@@ -68,19 +68,14 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 
 
+
 function updateCartQuantity() {
-        let cartQuantity = 0;
+    const cartQuantity = calculateCartQuantity();
 
-        cart.forEach((cartItem) => {
-            cartQuantity += cartItem.quantity;
-        });
-
-        document.querySelector('.js-cart-quantity')
-         .innerHTML = cartQuantity;
-}
-
-
-
+    document.querySelector('.js-cart-quantity')
+     .innerHTML = cartQuantity;
+};
+updateCartQuantity();
 
 
 document.querySelectorAll('.js-add-to-cart')
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
-import { cart, removeFromCart } from "../data/cart.js";  
+import { cart, removeFromCart, calculateCartQuantity,updateQuantity} from "../data/cart.js";  
 import { products } from "../data/products.js";    
 import {formatCurrency} from "./utils/money.js";
+
 
 
 let cartSummaryHTML = '';
@@ -39,13 +40,15 @@ cart.forEach((cartItem) =>{
             </div>
             <div class="product-quantity">
                 <span>
-                Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+                Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
                 </span>
-                <span class="update-quantity-link link-primary">
+                <span class="update-quantity-link link-primary js-update-link"
+                 data-product-id="${matchingProduct.id}">
                 Update
                 </span>
-                <span class="delete-quantity-link link-primary 
-                js-delete-link" data-product-id="${matchingProduct.id}">
+                <input class="quantity-input js-quantity-input-${matchingProduct.id}">
+                <span  class= "save-quantity-link  link-primary  js-save-link" data-product-id="${matchingProduct.id}">Save</span>
+                <span class="delete-quantity-link link-primary  js-delete-link" data-product-id="${matchingProduct.id}">
                 Delete
                 </span>
             </div>
@@ -118,6 +121,66 @@ document.querySelectorAll('.js-delete-link')
             `.js-cart-item-container-${productId}`
         );
         container.remove();
+
+        updateCartQuantity();
         
     })
-  })
+  });
+
+    function updateCartQuantity() {
+
+     const cartQuantity = calculateCartQuantity();
+
+         document.querySelector('.js-return-to-home-link')
+            .innerHTML = `${cartQuantity} items`;
+}
+updateCartQuantity();
+
+
+
+document.querySelectorAll('.js-update-link').forEach((link) =>{
+    link.addEventListener( 'click' , () => {
+         const productId = link.dataset.productId;
+
+         const container =  document.querySelector(
+         `.js-cart-item-container-${productId}`
+        );
+
+
+        container.classList.add('is-editing-quantity');
+
+       
+ 
+    })
+    
+})
+
+document.querySelectorAll('.js-save-link').forEach((link) =>{
+    link.addEventListener('click' , () => {
+        const productId = link.dataset.productId;
+        
+        const container =  document.querySelector(
+         `.js-cart-item-container-${productId}`
+        );
+
+         container.classList.remove('is-editing-quantity');
+
+         const quantityInput = document.querySelector(
+           `.js-quantity-input-${productId}` 
+         );
+
+         const newQuantity = Number(quantityInput.value);
+         updateQuantity(productId, newQuantity);
+
+
+        const quantityLabel = document.querySelector(
+        `.js-quantity-label-${productId}`
+          );
+        quantityLabel.innerHTML = newQuantity;
+
+         updateCartQuantity();
+
+         
+        
+    });
+})
