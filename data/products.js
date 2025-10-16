ccd33d82ef4ea194fb5f77e64bdde5e17685ca00
Xelias <bobkovdanil123@gmail.com>
2025-10-16 11:59:21 +0200

    Use inheritance and polymorohism ti add size chart

--- data/products.js
+++ data/products.js
@@ -40,7 +40,31 @@ class Product {
 
   }
 
+  extraInfoHTML(){
+    return '';
+  }
+
 }
+
+class Clothing extends Product {
+
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+  extraInfoHTML(){
+    return`
+    <a href="${this.sizeChartLink}" target="_blank">
+    Size chart
+    </a>
+    `
+  }
+}
+
+
+
 
 const product1 = new Product({
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -719,5 +743,8 @@ export const products = [
     ]
   }
 ].map((productDetails) =>{
+  if (productDetails.type === "clothing") {
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
